a2ffb9cf75b52d6400dd2be13574c226703abbe8
Soukaina Ouchen <souchen@e3r8p1.1337.ma>
2023-08-04 15:55:09 +0100

    stop here

--- chat-build/app/components/inputs/input.tsx
+++ chat-build/app/components/inputs/input.tsx
@@ -1,3 +1,21 @@
+'use client'
+
+// to avoid multiple className
+import clsx from 'clsx'
+import {FieldErrors, FieldValues, UseFormRegister} from 'react-hook-form'
+
+/****In Typescript, “?” represents optional parameters. We use optional parameters when it's not mandatory for that parameter to have a value or to be specified */
+
+interface InputProps {
+    label: string;
+    id: string;
+    type?: string;
+    requered?: boolean;
+    register: UseFormRegister<FieldValues>;
+    errors: FieldErrors;
+    disabled?: boolean;
+
+}
 const Input = () => {
     return (
         <div>
