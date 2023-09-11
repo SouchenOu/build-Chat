8cf758d219d75524a09c55cfaf4ca057a0c770fa
Soukaina Ouchen <souchen@e3r9p1.1337.ma>
2023-09-11 18:33:42 +0100

    still working on backend

---
+++ Chat-back_front/backend/src/auth/types.ts
@@ -0,0 +1,12 @@
+import {Field, ObjectType} from '@nestjs/graphql'
+
+@ObjectType()
+export class RegisterResponse {
+    @Field(() => UserActivation, {nullable: true})
+    user?: User;
+}
+@ObjectType()
+export class LoginResponse {
+    @Field(() => User)
+    user: User;
+}
