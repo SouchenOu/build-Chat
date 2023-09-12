ced4c8d47f827ea8a66eb21340a5f44d770f4123
Soukaina Ouchen <souchen@e3r9p1.1337.ma>
2023-09-12 09:46:23 +0100

    start working again

--- Chat-back_front/prisma-src/prisma/migrations/20230912082828_init/migration.sql
+++ Chat-back_front/prisma-src/prisma/migrations/20230912082828_init/migration.sql
@@ -1,15 +1,22 @@
--- CreateTable
-CREATE TABLE "User" (
-    "id" SERIAL NOT NULL,
-    "fullname" TEXT NOT NULL,
-    "email" TEXT NOT NULL,
-    "AvatarUrl" TEXT,
-    "password" TEXT NOT NULL,
-    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
-    "updateAt" TIMESTAMP(3) NOT NULL,
+/*
+  Warnings:
 
-    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
-);
+  - You are about to drop the column `name` on the `User` table. All the data in the column will be lost.
+  - A unique constraint covering the columns `[email]` on the table `User` will be added. If there are existing duplicate values, this will fail.
+  - Added the required column `email` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `fullname` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `password` to the `User` table without a default value. This is not possible if the table is not empty.
+  - Added the required column `updateAt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- AlterTable
+ALTER TABLE "User" DROP COLUMN "name",
+ADD COLUMN     "AvatarUrl" TEXT,
+ADD COLUMN     "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ADD COLUMN     "email" TEXT NOT NULL,
+ADD COLUMN     "fullname" TEXT NOT NULL,
+ADD COLUMN     "password" TEXT NOT NULL,
+ADD COLUMN     "updateAt" TIMESTAMP(3) NOT NULL;
 
 -- CreateTable
 CREATE TABLE "Chatroom" (
@@ -41,13 +48,13 @@ CREATE TABLE "_chatroomUser" (
 );
 
 -- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
-
--- CreateIndex
 CREATE UNIQUE INDEX "_chatroomUser_AB_unique" ON "_chatroomUser"("A", "B");
 
 -- CreateIndex
 CREATE INDEX "_chatroomUser_B_index" ON "_chatroomUser"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
 
 -- AddForeignKey
 ALTER TABLE "Message" ADD CONSTRAINT "Message_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
